5ad5c20e30fb1d4c1bb0d258d68a5aa482a3d853
androidunity@gmail.com <wanghl@hleh.org>
2016-11-26 11:04:22 +0800

    Moved BLE code to ThinkGearBLEService.java; Added interface for connect and data listener

--- ThinkGearBLE_AndroidStudio/app/src/main/java/com/chris/thinkgearble_androidstudio/DeviceDetailActivity.java
+++ ThinkGearBLE_AndroidStudio/app/src/main/java/com/chris/thinkgearble_androidstudio/DeviceDetailActivity.java
@@ -1,18 +1,40 @@
 package com.chris.thinkgearble_androidstudio;
 
 import android.app.Activity;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.widget.TextView;
 
 /**
  * Created by mac on 14/11/2016.
  */
 
-public class DeviceDetailActivity extends Activity {
+public class DeviceDetailActivity extends Activity implements ThinkGearBLEService.IThinkGearDataListener{
+    ThinkGearBLEService thinkGearService;
+    ThinkGearBLEService.MyBinder binder;
+
     private TextView poorSignalTextView;
     private TextView rawdataTextView;
     private TextView eSenseTextView;
     private TextView eegPowerTextView;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            binder = (ThinkGearBLEService.MyBinder)iBinder;
+            thinkGearService = binder.getThinkGearService();
+            thinkGearService.setThinkGearDataListener(DeviceDetailActivity.this);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +44,35 @@ public class DeviceDetailActivity extends Activity {
         rawdataTextView = (TextView)findViewById(R.id.rawdataTextView);
         eSenseTextView = (TextView)findViewById(R.id.eSenseTextView);
         eegPowerTextView = (TextView)findViewById(R.id.EEGPowerTextView);
+
+        Intent intent = new Intent(DeviceDetailActivity.this,ThinkGearBLEService.class);
+        bindService(intent,serviceConnection, Service.BIND_AUTO_CREATE);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        unbindService(serviceConnection);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+    }
+
+    public void onPoorSignalReceived(ThinkGearBLEService thinkGearBLEService, int poorSignal){
+        poorSignalTextView.setText("PoorSignal: " + poorSignal);
+    }
+    public void onRawdataReceived(ThinkGearBLEService thinkGearBLEService, int rawdata){
+        rawdataTextView.setText("Rawdata: " + rawdata);
+    }
+    public void onEEGPowerReceived(ThinkGearBLEService thinkGearBLEService, EEGPower eegPower){
+        eegPowerTextView.setText("Delta: " + eegPower.delta + "\nTheta: " + eegPower.theta +
+                "\nLowAlpha: " + eegPower.lowAlpha + "\nHighAlpha: " + eegPower.highAlpha +
+        "\nLowBeta: " + eegPower.lowBeta + "\nHighBeta: " + eegPower.highBeta +
+        "\nLowGamma: " + eegPower.lowGamma + "\nMiddleGamma: " + eegPower.middleGamma);
+    }
+    public void onESenseReceived(ThinkGearBLEService thinkGearBLEService, ESense eSense){
+        eSenseTextView.setText("Attention: " + eSense.attention + " Meditation: " + eSense.meditation);
     }
 }
